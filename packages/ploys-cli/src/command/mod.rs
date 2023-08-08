5ad18a287225d0e8787c9ee9f1d30912a2d03b27
Daniel Balcomb <daniel.balcomb@gmail.com>
2023-08-08 23:45:28 +0100

    Add inspect command (#2)

--- packages/ploys-cli/src/command/mod.rs
+++ packages/ploys-cli/src/command/mod.rs
@@ -1,13 +1,22 @@
+mod inspect;
+
 use anyhow::Error;
 use clap::Subcommand;
 
+use self::inspect::Inspect;
+
 /// The base command.
 #[derive(Subcommand)]
-pub enum Command {}
+pub enum Command {
+    /// Inspects the project.
+    Inspect(Inspect),
+}
 
 impl Command {
     /// Executes the command.
     pub fn exec(self) -> Result<(), Error> {
-        match self {}
+        match self {
+            Self::Inspect(inspect) => inspect.exec(),
+        }
     }
 }
