570ac87b53d51ba375eb8bf92c8dc1858e5e7425
Daniel Balcomb <daniel.balcomb@gmail.com>
2023-08-10 19:55:02 +0100

    Add support for inspecting remote repositories (#3)

--- packages/ploys-cli/tests/inspect.rs
+++ packages/ploys-cli/tests/inspect.rs
@@ -4,7 +4,8 @@ use assert_cmd::prelude::*;
 use predicates::prelude::*;
 
 #[test]
-fn test_inspect_command_output() {
+#[ignore]
+fn test_inspect_local_command_output() {
     Command::cargo_bin("ploys")
         .unwrap()
         .current_dir("../..")
@@ -13,4 +14,55 @@ fn test_inspect_command_output() {
         .success()
         .stdout(predicate::str::is_match(r#"Name:[ \t]*ploys"#).unwrap())
         .stdout(predicate::str::is_match(r#"Repository:.*github\.com/ploys/ploys"#).unwrap());
+}
+
+#[test]
+#[ignore]
+fn test_inspect_remote_command_output() {
+    Command::cargo_bin("ploys")
+        .unwrap()
+        .current_dir("../..")
+        .arg("inspect")
+        .arg("--remote")
+        .arg("ploys/ploys")
+        .assert()
+        .success()
+        .stdout(predicate::str::is_match(r#"Name:[ \t]*ploys"#).unwrap())
+        .stdout(predicate::str::is_match(r#"Repository:.*github\.com/ploys/ploys"#).unwrap());
+}
+
+#[test]
+#[ignore]
+fn test_inspect_remote_url_command_output() {
+    Command::cargo_bin("ploys")
+        .unwrap()
+        .current_dir("../..")
+        .arg("inspect")
+        .arg("--remote")
+        .arg("https://github.com/ploys/ploys")
+        .assert()
+        .success()
+        .stdout(predicate::str::is_match(r#"Name:[ \t]*ploys"#).unwrap())
+        .stdout(predicate::str::is_match(r#"Repository:.*github\.com/ploys/ploys"#).unwrap());
+
+    Command::cargo_bin("ploys")
+        .unwrap()
+        .current_dir("../..")
+        .arg("inspect")
+        .arg("--remote")
+        .arg("https://github.com/ploys/ploys.git")
+        .assert()
+        .success()
+        .stdout(predicate::str::is_match(r#"Name:[ \t]*ploys"#).unwrap())
+        .stdout(predicate::str::is_match(r#"Repository:.*github\.com/ploys/ploys"#).unwrap());
+
+    Command::cargo_bin("ploys")
+        .unwrap()
+        .current_dir("../..")
+        .arg("inspect")
+        .arg("--remote")
+        .arg("https://github.com/ploys/repo-not-found")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("Repository not found"));
 }
