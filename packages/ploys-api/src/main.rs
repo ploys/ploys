a674dfcbbe1b6d13e84c7314e9b099dc31fe6b9c
Daniel Balcomb <daniel.balcomb@gmail.com>
2024-06-07 21:29:15 +0100

    Add GitHub webhook endpoint

--- packages/ploys-api/src/main.rs
+++ packages/ploys-api/src/main.rs
@@ -1,13 +1,11 @@
-use axum::routing::get;
-use axum::Router;
+mod github;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+use axum::routing::post;
+use axum::Router;
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new().route("/github/webhook", post(self::github::webhook::receive));
 
     Ok(router.into())
 }
