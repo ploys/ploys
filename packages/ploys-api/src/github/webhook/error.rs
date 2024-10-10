ba2116ebe02c7c4225bc0c38a51569fb13fb1faa
Daniel Balcomb <daniel.balcomb@gmail.com>
2024-10-10 17:18:37 +0100

    Add automated release pull request creation

---
+++ packages/ploys-api/src/github/webhook/error.rs
@@ -0,0 +1,75 @@
+use std::borrow::Cow;
+use std::fmt::{self, Display};
+
+use axum::response::{IntoResponse, Response};
+use reqwest::StatusCode;
+
+/// The webhook response error.
+#[derive(Debug)]
+pub enum Error {
+    Payload,
+    Jwt(jsonwebtoken::errors::Error),
+    Request(reqwest::Error),
+    Project(ploys::project::Error),
+}
+
+impl Error {
+    pub fn status(&self) -> StatusCode {
+        match self {
+            Self::Payload => StatusCode::UNPROCESSABLE_ENTITY,
+            Self::Jwt(_) => StatusCode::FORBIDDEN,
+            Self::Request(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::Project(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+
+    pub fn message(&self) -> Cow<'static, str> {
+        match self {
+            Self::Payload => Cow::Borrowed("Unsupported or invalid payload"),
+            Self::Jwt(error) => Cow::Owned(format!("JWT: {error}")),
+            Self::Request(error) => Cow::Owned(format!("Request: {error}")),
+            Self::Project(error) => Cow::Owned(format!("Project: {error}")),
+        }
+    }
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        (self.status(), self.message()).into_response()
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Payload => None,
+            Self::Jwt(err) => Some(err),
+            Self::Request(err) => Some(err),
+            Self::Project(err) => Some(err),
+        }
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl From<jsonwebtoken::errors::Error> for Error {
+    fn from(error: jsonwebtoken::errors::Error) -> Self {
+        Self::Jwt(error)
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Self {
+        Self::Request(error)
+    }
+}
+
+impl From<ploys::project::Error> for Error {
+    fn from(error: ploys::project::Error) -> Self {
+        Self::Project(error)
+    }
+}
--- packages/ploys-api/src/github/webhook/mod.rs
+++ packages/ploys-api/src/github/webhook/mod.rs
@@ -1,21 +1,160 @@
+mod auth;
+mod error;
 mod header;
 mod payload;
 pub mod secret;
 
-use axum::http::StatusCode;
+use axum::extract::State;
+use ploys::project::source::revision::Revision;
+use ploys::project::Project;
+use reqwest::Client;
+use semver::Version;
+use serde::{Deserialize, Serialize};
 
-use self::payload::Payload;
+use crate::state::AppState;
+
+use self::auth::get_installation_access_token;
+use self::error::Error;
+use self::payload::{Payload, RefType};
 
 /// Receives the GitHub webhook event payload.
-pub async fn receive(payload: Payload) -> StatusCode {
-    println!("Event: {}", payload.event);
-    println!("Payload: {:#}", payload.value);
+pub async fn receive(state: State<AppState>, payload: Payload) -> Result<(), Error> {
+    match payload {
+        Payload::Create(payload) => match payload.ref_type {
+            RefType::Branch => {
+                if payload.r#ref.starts_with("release/") {
+                    create_release_pull_request(
+                        &payload.r#ref[8..],
+                        &payload.master_branch,
+                        payload.installation.id,
+                        payload.repository.id,
+                        &payload.repository.full_name,
+                        &state,
+                    )
+                    .await?;
+                }
 
-    StatusCode::OK
+                Ok(())
+            }
+            RefType::Tag => Ok(()),
+        },
+        Payload::Other(event, payload) => {
+            println!("Event: {event}");
+            println!("Payload: {payload:#}");
+
+            Ok(())
+        }
+    }
+}
+
+/// Creates a Pull Request for the given release.
+pub async fn create_release_pull_request(
+    release: &str,
+    target_branch: &str,
+    installation_id: u64,
+    repository_id: u64,
+    repository_name: &str,
+    state: &AppState,
+) -> Result<(), Error> {
+    let token = get_installation_access_token(installation_id, repository_id, state).await?;
+    let revision = Revision::branch(format!("release/{release}"));
+    let mut project =
+        Project::github_with_revision_and_authentication_token(repository_name, revision, &token)?;
+    let package = project.packages().iter().find(|package| {
+        release.starts_with(package.name())
+            && release.as_bytes().get(package.name().len()) == Some(&b'-')
+            && release[package.name().len() + 1..]
+                .parse::<Version>()
+                .is_ok()
+    });
+
+    let (package, version) = match package {
+        Some(package) => (
+            package.name().to_owned(),
+            release[package.name().len() + 1..]
+                .parse::<Version>()
+                .map_err(|_| Error::Payload)?,
+        ),
+        None => {
+            let version = release.parse::<Version>().map_err(|_| Error::Payload)?;
+            let package = project
+                .packages()
+                .iter()
+                .find(|package| package.name() == project.name())
+                .ok_or_else(|| ploys::project::Error::PackageNotFound(project.name().to_owned()))?;
+
+            (package.name().to_owned(), version)
+        }
+    };
+
+    let client = Client::new();
+    let message = match package == project.name() {
+        true => format!("Release `{version}`"),
+        false => format!("Release `{package}@{version}`"),
+    };
+
+    project.set_package_version(&package, version.clone())?;
+    project.commit(&message)?;
+
+    let issue_number = client
+        .post(format!(
+            "https://api.github.com/repos/{repository_name}/pulls"
+        ))
+        .header("Accept", "application/vnd.github+json")
+        .header("Authorization", format!("Bearer {token}"))
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("User-Agent", "ploys/ploys")
+        .json(&CreatePullRequest {
+            title: message,
+            head: format!("release/{release}"),
+            base: target_branch.to_owned(),
+            body: format!("Releasing package `{package}` version `{version}`."),
+        })
+        .send()
+        .await?
+        .json::<PullRequestResponse>()
+        .await?
+        .id;
+
+    client
+        .post(format!(
+            "https://api.github.com/repos/{repository_name}/issues/{issue_number}/labels"
+        ))
+        .header("Accept", "application/vnd.github+json")
+        .header("Authorization", format!("Bearer {token}"))
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("User-Agent", "ploys/ploys")
+        .json(&AddLabels {
+            labels: vec![String::from("release")],
+        })
+        .send()
+        .await?;
+
+    Ok(())
+}
+
+#[derive(Serialize)]
+struct CreatePullRequest {
+    title: String,
+    head: String,
+    base: String,
+    body: String,
+}
+
+#[derive(Deserialize)]
+struct PullRequestResponse {
+    id: u64,
+}
+
+#[derive(Serialize)]
+struct AddLabels {
+    labels: Vec<String>,
 }
 
 #[cfg(test)]
 mod tests {
+    use std::sync::Arc;
+
     use axum::body::Body;
     use axum::http::{Method, Request, StatusCode};
     use axum::routing::post;
@@ -25,14 +164,21 @@ mod tests {
     use sha2::Sha256;
     use tower_service::Service;
 
+    use crate::state::AppState;
+
     use super::secret::WebhookSecret;
 
     fn router() -> Router {
         Router::new().route(
             "/github/webhook",
-            post(super::receive).layer(Extension(WebhookSecret {
-                value: String::from("super_secret"),
-            })),
+            post(super::receive)
+                .layer(Extension(WebhookSecret {
+                    value: String::from("super_secret"),
+                }))
+                .with_state(AppState {
+                    github_app_client_id: Arc::from(""),
+                    github_app_private_key: Arc::from(""),
+                }),
         )
     }
 
