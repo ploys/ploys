242b2b2086772f13ec55b649d87e1124bb87c9c6
Daniel Balcomb <daniel.balcomb@gmail.com>
2024-10-29 13:49:17 +0000

    Add `package release` command

---
+++ packages/ploys-cli/src/package/release.rs
@@ -0,0 +1,49 @@
+use anyhow::Error;
+use clap::Args;
+use ploys::package::BumpOrVersion;
+use ploys::project::source::github::GitHub;
+use ploys::project::Project;
+
+use crate::util::repo_or_url::RepoOrUrl;
+
+/// The release command.
+#[derive(Args)]
+pub struct Release {
+    /// The package identifier.
+    package: String,
+
+    /// The package version or level (major, minor, patch, rc, beta, alpha).
+    version: BumpOrVersion,
+
+    /// The remote GitHub repository owner/repo or URL.
+    #[clap(long)]
+    remote: Option<RepoOrUrl>,
+
+    /// The authentication token for GitHub API access.
+    #[clap(long, env = "GITHUB_TOKEN", hide_env_values = true)]
+    token: String,
+}
+
+impl Release {
+    /// Executes the command.
+    pub fn exec(self) -> Result<(), Error> {
+        let remote = match self.remote {
+            Some(remote) => remote,
+            None => {
+                let project = Project::git(".")?;
+                let url = project.get_url()?;
+
+                RepoOrUrl::Url(url)
+            }
+        };
+
+        let project = Project::<GitHub>::github_with_authentication_token(
+            remote.try_into_repo()?.to_string(),
+            self.token,
+        )?;
+
+        project.initiate_package_release(self.package, self.version)?;
+
+        Ok(())
+    }
+}
