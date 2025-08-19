73566bd24c880d7094a16c6a687458d927207fd4
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-08-19 13:39:10 +0100

    Redesign `Memory` repository with `Arc` and `Mutex` (#258)

--- packages/ploys/src/repository/memory/mod.rs
+++ packages/ploys/src/repository/memory/mod.rs
@@ -1,28 +1,30 @@
 use std::collections::BTreeMap;
 use std::convert::Infallible;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 use bytes::Bytes;
+use parking_lot::Mutex;
 
 use super::Repository;
 
 /// An in-memory repository.
 #[derive(Clone)]
 pub struct Memory {
-    files: BTreeMap<PathBuf, Bytes>,
+    files: Arc<Mutex<BTreeMap<PathBuf, Bytes>>>,
 }
 
 impl Memory {
     /// Creates a new in-memory repository.
     pub fn new() -> Self {
         Self {
-            files: BTreeMap::new(),
+            files: Arc::new(Mutex::new(BTreeMap::new())),
         }
     }
 
     /// Inserts a file into the repository.
     pub fn insert_file(&mut self, path: impl Into<PathBuf>, file: impl Into<Bytes>) -> &mut Self {
-        self.files.insert(path.into(), file.into());
+        self.files.lock().insert(path.into(), file.into());
         self
     }
 
@@ -37,11 +39,18 @@ impl Repository for Memory {
     type Error = Infallible;
 
     fn get_file(&self, path: impl AsRef<Path>) -> Result<Option<Bytes>, Self::Error> {
-        Ok(self.files.get(path.as_ref()).cloned())
+        Ok(self.files.lock().get(path.as_ref()).cloned())
     }
 
     fn get_index(&self) -> Result<impl Iterator<Item = PathBuf>, Self::Error> {
-        Ok(self.files.keys().map(Clone::clone))
+        Ok(self
+            .files
+            .lock()
+            .keys()
+            .cloned()
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+            .into_iter())
     }
 }
 
