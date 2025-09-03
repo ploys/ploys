f618caca97318cf9d0bd40d8212645419b94e7a8
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-09-03 17:12:52 +0100

    Remove `Staging` repository infallible file methods (#282)

--- packages/ploys/tests/staging.rs
+++ packages/ploys/tests/staging.rs
@@ -1,16 +1,17 @@
 use ploys::project::Project;
+use ploys::repository::Stage;
 use ploys::repository::types::staging::Staging;
 use tempfile::tempdir;
 
 #[test]
 fn test_project_write() -> Result<(), Box<dyn std::error::Error>> {
     let repository = Staging::new()
-        .with_file("Ploys.toml", "[project]\nname = \"example\"")
-        .with_file("Cargo.toml", "[workspace]\nmembers = [\"packages/*\"]")
+        .with_file("Ploys.toml", "[project]\nname = \"example\"")?
+        .with_file("Cargo.toml", "[workspace]\nmembers = [\"packages/*\"]")?
         .with_file(
             "packages/example/Cargo.toml",
             "[package]\nname = \"example\"",
-        );
+        )?;
 
     let dir = tempdir()?;
     let project = Project::open(repository)?;
