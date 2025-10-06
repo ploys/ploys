70445757418b16d88e8153d2a5d110ec684b3167
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-10-06 17:15:16 +0100

    Add `package init` command (#297)

--- packages/ploys-cli/src/package/mod.rs
+++ packages/ploys-cli/src/package/mod.rs
@@ -1,8 +1,10 @@
+mod init;
 mod release;
 
 use anyhow::Error;
 use clap::{Args, Subcommand};
 
+use self::init::Init;
 use self::release::Release;
 
 /// The package command.
@@ -16,6 +18,7 @@ impl Package {
     /// Executes the package command.
     pub fn exec(self) -> Result<(), Error> {
         match self.command {
+            Command::Init(init) => init.exec(),
             Command::Release(release) => release.exec(),
         }
     }
@@ -24,6 +27,8 @@ impl Package {
 /// The inner package command.
 #[derive(Subcommand)]
 enum Command {
+    /// Initializes a new package.
+    Init(Init),
     /// Creates a new release.
     Release(Release),
 }
