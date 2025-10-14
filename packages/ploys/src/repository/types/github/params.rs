5bcf0e3785baa9cc54fbe3cd917a929cb5dd1fe9
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-10-14 13:55:11 +0100

    Implement `Commit` for `GitHub` repository

---
+++ packages/ploys/src/repository/types/github/params.rs
@@ -0,0 +1,30 @@
+/// The `GitHub` commit parameters.
+pub struct CommitParams {
+    message: String,
+}
+
+impl CommitParams {
+    /// Constructs new commit parameters with the given message.
+    pub fn new(message: impl Into<String>) -> Self {
+        Self {
+            message: message.into(),
+        }
+    }
+
+    /// Gets the commit message.
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+}
+
+impl From<&str> for CommitParams {
+    fn from(message: &str) -> Self {
+        Self::new(message)
+    }
+}
+
+impl From<String> for CommitParams {
+    fn from(message: String) -> Self {
+        Self::new(message)
+    }
+}
