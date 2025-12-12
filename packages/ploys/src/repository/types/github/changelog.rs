daa7645f321c632d3bd2d4f6342841e2ee71c0e5
Daniel Balcomb <daniel.balcomb@gmail.com>
2025-12-12 17:14:57 +0000

    Resolve `clippy::filter_next` lint (#317)

--- packages/ploys/src/repository/types/github/changelog.rs
+++ packages/ploys/src/repository/types/github/changelog.rs
@@ -171,8 +171,7 @@ fn get_previous_version(
     let previous_version = versions
         .iter()
         .filter(|previous_version| *previous_version < version)
-        .filter(|previous_version| version.pre.is_empty() || previous_version.pre.is_empty())
-        .next_back();
+        .rfind(|previous_version| version.pre.is_empty() || previous_version.pre.is_empty());
 
     match previous_version {
         Some(previous_version) => Some(previous_version.clone()),
