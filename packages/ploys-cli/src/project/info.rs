60bc9bd21bfffb29b42df960f993dab3096ebfcb
Daniel Balcomb <daniel.balcomb@gmail.com>
2026-03-05 16:31:13 +0000

    Remove `project info` command local and revision options

--- packages/ploys-cli/src/project/info.rs
+++ packages/ploys-cli/src/project/info.rs
@@ -1,38 +1,14 @@
-use std::path::PathBuf;
-
 use anyhow::Error;
 use clap::Args;
 use console::style;
 use ploys::project::Project;
-use ploys::repository::revision::Revision;
-use ploys::repository::{RepoAddr, Repository};
+use ploys::repository::RepoAddr;
 
 /// Gets the project information.
 #[derive(Args)]
 pub struct Info {
-    /// The target path.
-    #[arg(default_value = ".", conflicts_with_all = ["remote"])]
-    path: PathBuf,
-
-    /// The remote repository specification.
-    #[arg(long, conflicts_with_all = ["path"])]
-    remote: Option<RepoAddr>,
-
-    /// The repository head.
-    #[arg(long)]
-    head: bool,
-
-    /// The target branch name.
-    #[arg(long, conflicts_with_all = ["head", "tag", "sha"])]
-    branch: Option<String>,
-
-    /// The target tag name.
-    #[arg(long, conflicts_with_all = ["head", "branch", "sha"])]
-    tag: Option<String>,
-
-    /// The target commit SHA.
-    #[arg(long, conflicts_with_all = ["head", "branch", "tag"])]
-    sha: Option<String>,
+    /// The repository address (owner/name) or URL.
+    repo: RepoAddr,
 
     /// The authentication token for GitHub API access.
     #[arg(long, env = "GITHUB_TOKEN", hide_env_values = true)]
@@ -42,56 +18,11 @@ pub struct Info {
 impl Info {
     /// Executes the command.
     pub fn exec(self) -> Result<(), Error> {
-        match &self.remote {
-            Some(repo) => {
-                let revision = self.revision().unwrap_or_else(Revision::head);
-                let project = match &self.token {
-                    Some(token) => Project::github_with_revision_and_authentication_token(
-                        repo.clone(),
-                        revision,
-                        token,
-                    )?,
-                    None => Project::github_with_revision(repo.clone(), revision)?,
-                };
+        let project = match self.token {
+            Some(token) => Project::github_with_authentication_token(self.repo, token)?,
+            None => Project::github(self.repo)?,
+        };
 
-                self.print(project)?;
-
-                Ok(())
-            }
-            None => {
-                if let Some(revision) = self.revision() {
-                    let project = Project::git_with_revision(&self.path, revision)?;
-
-                    self.print(project)?;
-                } else {
-                    let project = Project::fs(&self.path)?;
-
-                    self.print(project)?;
-                }
-
-                Ok(())
-            }
-        }
-    }
-
-    /// Gets the Git revision.
-    pub fn revision(&self) -> Option<Revision> {
-        match self.head {
-            true => Some(Revision::head()),
-            false => match &self.branch {
-                Some(branch) => Some(Revision::branch(branch)),
-                None => match &self.tag {
-                    Some(tag) => Some(Revision::tag(tag)),
-                    None => self.sha.as_ref().map(Revision::sha),
-                },
-            },
-        }
-    }
-
-    pub fn print<T>(&self, project: Project<T>) -> Result<(), Error>
-    where
-        T: Repository,
-    {
         println!("{}:\n", style("Project").underlined().bold());
         println!("Name:        {}", project.name());
 
