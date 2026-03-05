d5b391a38531a965c08b88a9725028c33fa9d8c8
Daniel Balcomb <daniel.balcomb@gmail.com>
2026-03-05 20:42:30 +0000

    Remove `project info` command path and revision options (#337)

--- packages/ploys-cli/tests/project_info.rs
+++ packages/ploys-cli/tests/project_info.rs
@@ -4,49 +4,13 @@ use assert_cmd::prelude::*;
 use predicates::prelude::*;
 
 #[test]
-fn test_project_info_command_fs_output() {
-    Command::cargo_bin("ploys")
-        .unwrap()
-        .current_dir("../..")
-        .arg("project")
-        .arg("info")
-        .assert()
-        .success()
-        .stdout(predicate::str::is_match(r#"Name:[ \t]*ploys"#).unwrap())
-        .stdout(predicate::str::is_match(r#"Repository:.*ploys/ploys"#).unwrap());
-}
-
-#[test]
-fn test_project_info_command_git_output() {
-    Command::cargo_bin("ploys")
-        .unwrap()
-        .current_dir("../..")
-        .arg("project")
-        .arg("info")
-        .arg("--head")
-        .assert()
-        .success()
-        .stdout(predicate::str::is_match(r#"Name:[ \t]*ploys"#).unwrap())
-        .stdout(predicate::str::is_match(r#"Repository:.*ploys/ploys"#).unwrap());
-}
-
-#[test]
 #[ignore]
-fn test_project_info_command_github_output() {
-    let mut command = Command::cargo_bin("ploys").unwrap();
-
-    command
-        .current_dir("../..")
+fn test_project_info_command() {
+    Command::cargo_bin("ploys")
+        .unwrap()
         .arg("project")
         .arg("info")
-        .arg("--remote")
-        .arg("https://github.com/ploys/ploys");
-
-    if let Ok(sha) = std::env::var("GITHUB_SHA") {
-        command.arg("--sha").arg(sha);
-    }
-
-    command
+        .arg("ploys/ploys")
         .assert()
         .success()
         .stdout(predicate::str::is_match(r#"Name:[ \t]*ploys"#).unwrap())
@@ -54,11 +18,9 @@ fn test_project_info_command_github_output() {
 
     Command::cargo_bin("ploys")
         .unwrap()
-        .current_dir("../..")
         .arg("project")
         .arg("info")
-        .arg("--remote")
-        .arg("https://github.com/ploys/repo-not-found")
+        .arg("ploys/repo-not-found")
         .assert()
         .failure()
         .stderr(predicate::str::contains("Not Found"));
