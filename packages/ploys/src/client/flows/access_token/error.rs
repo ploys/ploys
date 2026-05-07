5068f74ab1cfa51d2e99ea1b5afa0a96c2dc9e6f
Daniel Balcomb <daniel.balcomb@gmail.com>
2026-05-07 14:21:31 +0100

    Add user field to client credentials

--- packages/ploys/src/client/flows/access_token/error.rs
+++ packages/ploys/src/client/flows/access_token/error.rs
@@ -7,14 +7,23 @@ use crate::client::TokenType;
 pub enum Error {
     /// An unsupported token type.
     UnsupportedTokenType(TokenType),
+    /// A request error.
+    Request(reqwest::Error),
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::UnsupportedTokenType(ty) => write!(f, "Unsupported token type: {ty}"),
+            Self::Request(err) => Display::fmt(err, f),
         }
     }
 }
 
 impl std::error::Error for Error {}
+
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Self::Request(err)
+    }
+}
